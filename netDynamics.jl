9d00e85f84f2355a3e1160c78f74250ec90ff2a9
mantzaris <avmantzaris@gmail.com>
2017-09-27 17:25:49 -0400

    time stampled basal rate now in

--- netDynamics.jl
+++ netDynamics.jl
@@ -1,19 +1,20 @@
-function dynamicCommunicators(NN,tt)
+function dynamicCommunicators(NN,TT)
     pb = 0.25
     cb = 1
     cr = 4
-    AA = zeros(NN,NN,tt)
+    AA = zeros(NN,NN,TT)
     #produce the data
     #produce the Basal rate in the matrix
-    for ii in 1:NN
-        if(rand(1)[1] < pb)#produce Basal
-            tmp = deleteat!(collect(1:NN),ii)
-            destinationN = tmp[rand(1:end)]            
-            AA[ii,destinationN,1] = 1
+    for tt in 1:TT
+        for ii in 1:NN
+            if(rand(1)[1] < pb)#produce Basal
+                tmp = deleteat!(collect(1:NN),ii)
+                destinationN = tmp[rand(1:end)]            
+                AA[ii,destinationN,tt] = 1
+            end
+
         end
-        
     end
-    
     print(AA)
 
 end
