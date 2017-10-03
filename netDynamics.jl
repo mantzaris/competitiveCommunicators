ae9d9b32cd25d4b45492742c9debf4cc6df66399
mantzaris <avmantzaris@gmail.com>
2017-10-02 20:08:12 -0400

    changed the dynCen still not `00

--- netDynamics.jl
+++ netDynamics.jl
@@ -1,5 +1,6 @@
-function dynamicCommunicators(NN=40,TT=365,pb=0.501,cb=1,cr=4)
-    imp = [e^x for x in 1:NN]
+function dynamicCommunicators(NN=40,TT=365,pb=0.1,cb=1,cr=4)
+    imp = [x^4 for x in 1:NN]
+    println(imp)
     AA = zeros(NN,NN,TT)
     #init Data
     for ii in 1:NN
@@ -16,6 +17,7 @@ function dynamicCommunicators(NN=40,TT=365,pb=0.501,cb=1,cr=4)
         #Basal Loop
         for ii in 1:NN
             if(rand(1)[1] < pb)
+                print("basal")
                 tmp = deleteat!(collect(1:NN),ii)
                 destinationN = tmp[rand(1:end)]            
                 AA[ii,destinationN,tt+1] = 1
@@ -24,13 +26,17 @@ function dynamicCommunicators(NN=40,TT=365,pb=0.501,cb=1,cr=4)
         end
         #Response Loop
         for ii in 1:NN
-            msgsTo_ii = AA[:,ii]
+            msgsTo_ii = AA[:,ii,tt]
+            
             totalImportance = sum(msgsTo_ii .* imp)
+            
             r_nNumerator = totalImportance
-            r_nDenominator = 1 + (imp[end] * sum(msgsTo_ii))
+            r_nDenominator = 1 + (findmax(imp)[1] * sum(msgsTo_ii))
             r_next = r_nNumerator / r_nDenominator
-            if(r_next > rand(1)[1])#if so generate cr links
-                #println("response")
+            println(r_next)
+            if(r_next >  rand(1)[1])#if so generate cr links
+                print("response=");print(find(totalImportance));println(":")
+                # println(msgsTo_ii)
                 tmp = deleteat!(collect(1:NN),ii)
                 destinationNodes = tmp[randperm(length(tmp))[1:cr]]
                 AA[ii,destinationNodes,tt+1] = 1
@@ -39,33 +45,46 @@ function dynamicCommunicators(NN=40,TT=365,pb=0.501,cb=1,cr=4)
         end
                 
     end
-
+    #print(AA)
     #now get the total out degree of the nodes
     outDegVec = sum(sum(AA,3),2)
-    
+    dynamicCentrality(AA,NN,TT)
+end
 
+
+
+function dynamicCentrality(AA,NN,TT)
+    println(":")
     daynorm = zeros(TT,1);#spectrum of eigenvalues
     for ii in 1:TT
         Atemp = AA[:,:,ii]
         daynorm[ii] = findmax(abs(eigvals(Atemp)))[1]
-    end
-    
+    end    
     #compute the alpha to the value we want to compute B
-    alpha = 0.9*(1/findmax(daynorm)[1]);#90percent of the max
-    print(alpha)
-    #alpha = 0.25;
+    #alpha = 0.9*(1/findmax(daynorm)[1]);#90percent of the max
+    #println(alpha)
+    alpha = 0.65;
 
     BB = eye(NN,NN);
-
-    for k = TT:-1:1
-        Aday = AA[:,:,k]
-        BB = (eye(NN,NN) - alpha*Aday)\BB
-        BB = abs(BB)
-        BB = BB/norm(BB)
-
+    for kk = 1:TT#:-1:1
+        Aday = AA[:,:,kk]
+        println(Aday)
+        BBtmp = inv(eye(NN,NN) - alpha*Aday)#\BB
+        #BBtmp = (eye(NN,NN) - alpha*Aday) \ BB
+        BBtmp = abs(BBtmp)
+        #BB = BBtmp / norm(BBtmp)
+        BB = (BB*BBtmp) / norm(BB)
     end
     #sum across columns
-    Cbroad = sum(BB',2)
-    
-    return Cbroad
+    Cbroad = sum(BB',2)    
+    println(Cbroad)
+    println(0.9*(1/findmax(daynorm)[1]))
 end
+
+
+#for k = size(Atensor,3):-1:1,
+#    Aday = Atensor(:,:,k);
+#    B = (eye(size(Atensor,1),size(Atensor,1)) - alpha*Aday)\B;
+#    B = abs(B);
+#    B = B/norm(B);
+#end
